f23328b8117c0f3fd09fae38cf0d2245da16aa3b
NoOneLevi <zxvitor1@hotmail.com>
2024-11-08 17:14:11 -0300

    first commit

---
+++ fibonacci.java
@@ -0,0 +1,51 @@
+public class fibonacci {
+    public static int fiboRecursive(int n) {
+        // Aqui eu testo para ver se o argumento N é valido
+        if (n < 0) {
+            throw new IllegalArgumentException("Numero deve maior ou igual a zero.");
+        }
+        // Como no Fibonacci se N for 0 ou 1 ele é igual
+        // a 0 ou 1 respectivamente
+        if (n == 0) {
+            return n;
+        }
+        if (n == 1) {
+            return n;
+        }
+        // Como é uma função recursiva estou chamando elas
+        // Passando um N inteiro menos 1
+        // Somando com um N inteiro menos 2 dele e retornando
+        return fiboRecursive(n - 1) + fiboRecursive(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fiboRecursive(10));
+        System.out.println(fiboLinear(10));
+    }
+
+    public static int fiboLinear(int n) {
+        // Aqui eu testo para ver se o argumento N é valido
+        if (n < 0) {
+            throw new IllegalArgumentException("Numero deve ser maior ou igual a zero.");
+        }
+        // Como no Fibonacci se N for 0 ou 1 ele é igual
+        // a 0 ou 1 respectivamente
+        if (n == 0) {
+            return n;
+        }
+        if (n == 1) {
+            return n;
+        }
+        int term1 = 0, term2 = 1;
+        // Estou iterando sobre N inteiro, guardando o valor do proximo
+        // Termo em nextTerm com a soma dos dos dois ultimos termos
+        // assim fazendo a troca de valores em cada iteração
+        // term1 recebendo term2 e term2 recebendo o nextTerm
+        for (int i = 2; i < n; i++) {
+            int nextTerm = term1 + term2;
+            term1 = term2;
+            term2 = nextTerm;
+        }
+        return term2;
+    }
+}
---
+++ primeNumbers.java
@@ -0,0 +1,50 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class primeNumbers {
+    public static void main(String[] args) {
+        System.out.println(primesNumbersRecursive(10));
+        System.out.println(primesNumbersLinear(10));
+    }
+
+    public static List<Integer> primesNumbersRecursive(int n) {
+        if (n < 2) {
+            return new ArrayList<>();
+        }
+        // Aqui eu fa;o a recursvidade construir a lista de numeros primos
+        // cada chamada calcula os numeros primos para um valor menor q n
+        // ate chegar ao teste n < 2
+        List<Integer> primes = primesNumbersRecursive(n - 1);
+        if (isPrime(n, primes)) {
+            primes.add(n);
+        }
+        return primes;
+    }
+
+    public static List<Integer> primesNumbersLinear(int n) {
+        List<Integer> primes = new ArrayList<>();
+        if (n < 2) {
+            return new ArrayList<>();
+        }
+        // aqui eu uso o for para verificar cada numero menor que n
+        // para saber quais são os numeros primos
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i, primes)) {
+                primes.add(i);
+            }
+        }
+        return primes;
+    }
+
+    public static boolean isPrime(int n, List<Integer> primes) {
+        if (n <= 1) {
+            throw new IllegalArgumentException("Apenas numero maior que 1 sao permitidos");
+        }
+        for (int prime : primes) {
+            if (n % prime == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
